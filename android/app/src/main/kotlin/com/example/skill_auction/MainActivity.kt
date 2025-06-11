71542bd9e8399194e2c21becaec2d281186d627a
humaira <humairaabdullah579@gmail.com>
2025-06-11 10:26:06 -0700

    skill

--- android/app/src/main/kotlin/com/example/skill_auction/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/skill_auction/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.skill_auction
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
